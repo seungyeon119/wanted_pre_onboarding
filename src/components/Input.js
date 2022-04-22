39e30445a2ae93e1292d9142d69185fd2826cb93
seungyeon119 <ssy01191@gmail.com>
2022-04-23 00:58:14 +0900

    이메일 형식에 맞는지 확인하기

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+
+import { ReactComponent as CheckSvg } from '../assets/check_circle.svg';
 
 const Container = styled.div``;
 
@@ -6,18 +9,39 @@ const StyledInput = styled.input``;
 
 const StyledLabel = styled.label``;
 
+const CheckIcon = styled(CheckSvg)`
+  fill: ${({ isValid }) => (isValid ? '#10AFAF' : '#d3d3d3')};
+`;
+
 function Input() {
+  const [email, setEmail] = useState('');
+  const [isValid, setIsValid] = useState(false);
+
+  const validateEmail = (value) => {
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (emailRegex.test(value)) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  };
+  const handleEmail = ({ target: { value } }) => {
+    setEmail(value);
+    validateEmail(value);
+  };
+
   return (
-    <>
+    <form>
       <Container>
         <StyledLabel htmlFor="email">E-mail</StyledLabel>
-        <StyledInput id="email" type="email" placeholder="Email" />
+        <StyledInput id="email" type="email" placeholder="Email" onChange={handleEmail} />
+        <CheckIcon isValid={isValid} />
       </Container>
       <Container>
         <StyledLabel htmlFor="password">Password</StyledLabel>
         <StyledInput id="password" placeholder="Password" />
       </Container>
-    </>
+    </form>
   );
 }
 
