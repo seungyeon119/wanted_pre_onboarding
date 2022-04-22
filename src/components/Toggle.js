1911b4da6e94adb8d5cbb2a8dde0e8965ca1d859
seungyeon119 <ssy01191@gmail.com>
2022-04-22 23:11:36 +0900

    버튼 클릭에 따른 상태 변화

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div``;
 
-const Button = styled.button``;
+const Button = styled.button`
+  color: ${({ clicked }) => (clicked ? '#4f5559' : 'gray')};
+`;
 
 function Toggle() {
+  const [isToggled, setIsToggled] = useState(false);
+  const handleClick = (state) => {
+    setIsToggled(state);
+  };
   return (
     <Container>
-      <Button>기본</Button>
-      <Button>상세</Button>
+      <Button onClick={() => handleClick(false)} clicked={!isToggled}>
+        기본
+      </Button>
+      <Button onClick={() => handleClick(true)} clicked={isToggled}>
+        상세
+      </Button>
     </Container>
   );
 }
