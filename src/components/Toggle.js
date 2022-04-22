9b67bd06694f032d5d929a268bb28bb1b569b840
seungyeon119 <ssy01191@gmail.com>
2022-04-22 23:39:00 +0900

    토글 상태 보여주는 <ToggleSelector> 추가

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,10 +1,29 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 400px;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #d3d3d3;
+`;
 
 const Button = styled.button`
+  z-index: 100;
+  width: 50%;
+  font-weight: 700;
+  background-color: rgba(0, 0, 0, 0);
   color: ${({ clicked }) => (clicked ? '#4f5559' : 'gray')};
+`;
+
+const ToggleSelector = styled.div`
+  position: absolute;
+  width: 50%;
+  height: 90%;
+  background-color: #fff;
+  transform: ${({ isToggled }) => (isToggled ? 'translate(100%, 0)' : 'none')};
 `;
 
 function Toggle() {
@@ -20,6 +39,7 @@ function Toggle() {
       <Button onClick={() => handleClick(true)} clicked={isToggled}>
         상세
       </Button>
+      <ToggleSelector isToggled={isToggled} />
     </Container>
   );
 }
